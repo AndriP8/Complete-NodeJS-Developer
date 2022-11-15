0045e0fdc613da73a2c8cf94ce74ab284a12eef7
AndriP8 <andrikcoc26@gmail.com>
2022-11-15 20:24:37 +0700

    express-project: route parametes

--- express-project/server.js
+++ express-project/server.js
@@ -4,11 +4,31 @@ const app = express();
 
 const PORT = 3000;
 
-app.get("/", (req, res) => {
-  res.send({
+const friends = [
+  {
+    id: 0,
+    name: "Albert Einstein"
+  },
+  {
     id: 1,
     name: "Isaac Newton"
-  });
+  }
+];
+
+app.get("/friends", (req, res) => {
+  res.json(friends);
+});
+
+app.get("/friends/:friendId", (req, res) => {
+  const friendId = Number(req.params.friendId);
+  const friend = friends[friendId];
+  if (friend) {
+    res.status(200).json(friend);
+  } else {
+    res.status(404).json({
+      error: "Friend does not exist"
+    });
+  }
 });
 
 app.get("/message", (req, res) => {
