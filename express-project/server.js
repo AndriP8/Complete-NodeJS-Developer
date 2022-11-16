35cf0e87caa54eaf0088c1bf3ce9e0c5bfb36d7d
AndriP8 <andrikcoc26@gmail.com>
2022-11-16 15:58:39 +0700

    express-project: POST request

--- express-project/server.js
+++ express-project/server.js
@@ -15,11 +15,22 @@ const friends = [
   }
 ];
 
-app.use((req, res, next) => {
-  const start = Date.now();
-  next();
-  const delta = Date.now() - start;
-  console.log(`${req.method} ${req.url} ${delta}ms`);
+app.use(express.json());
+
+app.post("/friends", (req, res) => {
+  if (!req.body.name) {
+    res.status(400).json({
+      error: "Missing friend name"
+    });
+  } else {
+    const newFriend = {
+      id: friends.length + 1,
+      name: req.body.name
+    };
+    friends.push(newFriend);
+
+    res.json(newFriend);
+  }
 });
 
 app.get("/friends", (req, res) => {
