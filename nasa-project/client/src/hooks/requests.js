272358bbdb37641544b381ff2a3712f25ff7c580
AndriP8 <andrikcoc26@gmail.com>
2022-11-18 21:13:41 +0700

    nasa-project: GET planets

--- nasa-project/client/src/hooks/requests.js
+++ nasa-project/client/src/hooks/requests.js
@@ -1,4 +1,8 @@
+const API_URL = "http://localhost:8000";
+
 async function httpGetPlanets() {
+  const response = await fetch(`${API_URL}/planets`);
+  return await response.json();
   // TODO: Once API is ready.
   // Load planets and return as JSON.
 }
@@ -18,9 +22,4 @@ async function httpAbortLaunch(id) {
   // Delete launch with given ID.
 }
 
-export {
-  httpGetPlanets,
-  httpGetLaunches,
-  httpSubmitLaunch,
-  httpAbortLaunch,
-};
+export { httpGetPlanets, httpGetLaunches, httpSubmitLaunch, httpAbortLaunch };
