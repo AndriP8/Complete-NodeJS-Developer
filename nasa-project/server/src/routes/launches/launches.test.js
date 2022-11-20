0ff43a409cb5aa6d3cb34e31266b2f62c6a33dda
AndriP8 <andrikcoc26@gmail.com>
2022-11-20 20:44:51 +0700

    nasa-project: testing POST api

--- nasa-project/server/src/routes/launches/launches.test.js
+++ nasa-project/server/src/routes/launches/launches.test.js
@@ -3,7 +3,38 @@ const app = require("../../app");
 
 describe("Test GET /launches", () => {
   test("should response with 200 success", async () => {
-    const response = await request(app).get("/launches");
-    expect(response.statusCode).toBe(200);
+    await request(app)
+      .get("/launches")
+      .expect("Content-Type", /json/)
+      .expect(200);
+  });
+});
+
+describe("Test POST /launches", () => {
+  test("should response with 201 success", async () => {
+    const completeLaucnhDate = {
+      mission: "USS Enterprise",
+      rocket: "NCC 1701-D",
+      target: "Kepler-186 f",
+      launchDate: "January 4, 2028"
+    };
+
+    const launchDataWithoutDate = {
+      mission: "USS Enterprise",
+      rocket: "NCC 1701-D",
+      target: "Kepler-186 f"
+    };
+
+    const response = await request(app)
+      .post("/launches")
+      .send(completeLaucnhDate)
+      .expect("Content-Type", /json/)
+      .expect(201);
+
+    const requestDate = new Date(completeLaucnhDate.launchDate).valueOf();
+    const responseDate = new Date(response.body.launchDate).valueOf();
+
+    expect(responseDate).toBe(requestDate);
+    expect(response.body).toMatchObject(launchDataWithoutDate);
   });
 });
