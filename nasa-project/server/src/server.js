cd1fe866c6b10f416804157c357d0b6bd0e2be6a
AndriP8 <andrikcoc26@gmail.com>
2022-11-24 21:10:54 +0700

    databases: adding schema for nasa project

--- nasa-project/server/src/server.js
+++ nasa-project/server/src/server.js
@@ -1,5 +1,6 @@
 const http = require("http");
 const app = require("./app");
+const mongoose = require("mongoose");
 
 const { loadPlanetsData } = require("./models/planets.models");
 
@@ -7,7 +8,20 @@ const server = http.createServer(app);
 
 const PORT = process.env.PORT || 8000;
 
+const MONGO_URL =
+  "mongodb+srv://nasa-api:2L11DPUQBbAQLpPu@cluster0.iojat2w.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+mongoose.connection.once("open", () => {
+  console.log("MongoDB connection ready");
+});
+
+mongoose.connection.on("error", (error) => {
+  console.log(`MongoDB connection ${error}`);
+});
+
 const startServer = async () => {
+  await mongoose.connect(MONGO_URL);
+
   await loadPlanetsData();
 
   server.listen(PORT, () => {
