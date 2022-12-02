d499f8da635baf4d0b08317488d6d8869b6af7c5
AndriP8 <andrikcoc26@gmail.com>
2022-12-02 16:37:29 +0700

    security: implement google OAuth

--- security-example/server.js
+++ security-example/server.js
@@ -3,14 +3,92 @@ const path = require("path");
 const express = require("express");
 const https = require("https");
 const helmet = require("helmet");
+const passport = require("passport");
+const cookieSession = require("cookie-session");
+const { Strategy } = require("passport-google-oauth20");
+
+require("dotenv").config();
 
 const PORT = 3000;
+
+const config = {
+  CLIENT_ID: process.env.CLIENT_ID,
+  CLIENT_SECRET: process.env.CLIENT_SECRET,
+  COOKIE_KEY_1: process.env.COOKIE_KEY_1,
+  COOKIE_KEY_2: process.env.COOKIE_KEY_2
+};
+
+const AUTH_OPTIONS = {
+  callbackURL: "/auth/google/callback",
+  clientID: config.CLIENT_ID,
+  clientSecret: config.CLIENT_SECRET
+};
+
+const verifyCallback = (accessToken, refreshToken, profile, done) => {
+  console.log("Google profile", profile);
+  done(null, profile);
+};
+
+passport.use(new Strategy(AUTH_OPTIONS, verifyCallback));
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((obj, done) => {
+  done(null, obj);
+});
 
 const app = express();
 
 app.use(helmet());
+app.use(
+  cookieSession({
+    name: "session",
+    maxAge: 24 * 60 * 60 * 1000,
+    keys: [config.COOKIE_KEY_1, config.COOKIE_KEY_2]
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.get("/", (req, res) => {
+const checkLoggedIn = (req, res, next) => {
+  const isLoggedIn = true;
+  if (!isLoggedIn) {
+    return res.status(401).json({
+      error: "You must log in"
+    });
+  }
+  next();
+};
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["email"]
+  }),
+  (req, res) => {}
+);
+
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", {
+    failureRedirect: "/failure",
+    successRedirect: "/",
+    session: true
+  }),
+  (req, res) => {
+    console.log("Google called us back!");
+  }
+);
+
+app.get("/auth/logout", (req, res) => {});
+
+app.get("/failure", (req, res) => {
+  return res.send("Failed to log in!");
+});
+
+app.get("/", checkLoggedIn, (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
