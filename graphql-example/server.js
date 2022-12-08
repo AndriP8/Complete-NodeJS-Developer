6261fa6a9bee12f729ab56a49c027a6f5e7ac5c0
AndriPurnomo <andri.adrp@gmail.com>
2022-12-08 22:01:39 +0700

    graphql: building an apollo server

--- graphql-example/server.js
+++ graphql-example/server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { graphqlHTTP } = require("express-graphql");
+const { ApolloServer } = require("apollo-server-express");
 const { makeExecutableSchema } = require("@graphql-tools/schema");
 const { loadFilesSync } = require("@graphql-tools/load-files");
 const path = require("path");
@@ -7,21 +7,24 @@ const path = require("path");
 const typesArray = loadFilesSync(path.join(__dirname, "**/*.graphql"));
 const resolversArray = loadFilesSync(path.join(__dirname, "**/*.resolvers.js"));
 
-const schema = makeExecutableSchema({
-  typeDefs: typesArray,
-  resolvers: resolversArray
-});
+const startApolloServer = async () => {
+  const app = express();
 
-const app = express();
+  const schema = makeExecutableSchema({
+    typeDefs: typesArray,
+    resolvers: resolversArray
+  });
 
-app.use(
-  "/graphql",
-  graphqlHTTP({
-    schema: schema,
-    graphiql: true
-  })
-);
+  const server = new ApolloServer({
+    schema: schema
+  });
 
-app.listen(3000, () => {
-  console.log("listen on port 3000");
-});
+  await server.start();
+  server.applyMiddleware({ app, path: "/graphql" });
+
+  app.listen(3000, () => {
+    console.log("listen on port 3000");
+  });
+};
+
+startApolloServer();
