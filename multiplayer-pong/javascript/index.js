561a708012b3181504c8df5550d07f02752d8b2f
AndriPurnomo <andri.adrp@gmail.com>
2022-12-09 20:16:35 +0700

    socket: handling event

--- multiplayer-pong/javascript/index.js
+++ multiplayer-pong/javascript/index.js
@@ -2,6 +2,7 @@
 const canvas = document.createElement("canvas");
 const context = canvas.getContext("2d");
 const socket = io("http://localhost:3000");
+let isReferee = false;
 let paddleIndex = 0;
 
 let width = 500;
@@ -160,12 +161,15 @@ function animate() {
   window.requestAnimationFrame(animate);
 }
 
-// Start Game, Reset Everything
-function startGame() {
+// Load Game, Reset Everything
+function loadGame() {
   createCanvas();
   renderIntro();
+  socket.emit("ready");
+}
 
-  paddleIndex = 0;
+function startGame() {
+  paddleIndex = isReferee ? 0 : 1;
   window.requestAnimationFrame(animate);
   canvas.addEventListener("mousemove", (e) => {
     playerMoved = true;
@@ -182,4 +186,15 @@ function startGame() {
 }
 
 // On Load
-startGame();
+loadGame();
+
+socket.on("connect", () => {
+  console.log("Connected as", socket.id);
+});
+
+socket.on("startGame", (refereeId) => {
+  console.log("Referee as", refereeId);
+
+  isReferee = socket.id === refereeId;
+  startGame();
+});
