0a53203a80a6aab5e7d6b1e2dfc0e545f8a492fe
AndriPurnomo <andri.adrp@gmail.com>
2022-12-09 20:33:14 +0700

    socket: implementing game logic

--- multiplayer-pong/javascript/index.js
+++ multiplayer-pong/javascript/index.js
@@ -90,6 +90,10 @@ function ballReset() {
   ballX = width / 2;
   ballY = height / 2;
   speedY = 3;
+  socket.emit("ballMove", {
+    ballX,
+    ballY
+  });
 }
 
 // Adjust Ball Movement
@@ -100,6 +104,11 @@ function ballMove() {
   if (playerMoved) {
     ballX += speedX;
   }
+  socket.emit("ballMove", {
+    ballX,
+    ballY,
+    score
+  });
 }
 
 // Determine What Ball Bounces Off, Score Points, Reset Ball
@@ -155,9 +164,11 @@ function ballBoundaries() {
 
 // Called Every Frame
 function animate() {
-  ballMove();
+  if (isReferee) {
+    ballMove();
+    ballBoundaries();
+  }
   renderCanvas();
-  ballBoundaries();
   window.requestAnimationFrame(animate);
 }
 
@@ -180,6 +191,9 @@ function startGame() {
     if (paddleX[paddleIndex] > width - paddleWidth) {
       paddleX[paddleIndex] = width - paddleWidth;
     }
+    socket.emit("paddleMove", {
+      xPosition: paddleX[paddleIndex]
+    });
     // Hide Cursor
     canvas.style.cursor = "none";
   });
@@ -197,4 +211,13 @@ socket.on("startGame", (refereeId) => {
 
   isReferee = socket.id === refereeId;
   startGame();
+});
+
+socket.on("paddleMove", (paddleData) => {
+  const opponentPaddleIndex = 1 - paddleIndex;
+  paddleX[opponentPaddleIndex] = paddleData.xPosition;
+});
+
+socket.on("ballMove", (ballData) => {
+  ({ ballX, ballY, score } = ballData);
 });
--- multiplayer-pong/server.js
+++ multiplayer-pong/server.js
@@ -20,4 +20,12 @@ io.on("connection", (socket) => {
       io.emit("startGame", socket.id);
     }
   });
+
+  socket.on("paddleMove", (paddleData) => {
+    socket.broadcast.emit("paddleMove", paddleData);
+  });
+
+  socket.on("ballMove", (ballData) => {
+    socket.broadcast.emit("ballMove", ballData);
+  });
 });
